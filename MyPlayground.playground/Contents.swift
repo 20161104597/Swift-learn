c851c20751c3afadfb809b68ea89c1f167bed1e0
20161104597 <Stereophonic@admindeiMac-38.local>
2018-09-19 10:50:35 +0800

    1
    1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("hello world")
+//let x = 4
+//let y = 5
+//print(x+y)
+/*let sumiiya = "girlfriend"
+var 开销 : Int = 50
+var 水 : Double = 3.5
+9 % 4
+let a = true
+ */
+/*var sum = 0
+for num in 0...100
+{
+    sum = sum + num
+}
+print(sum)
+
+//var a = [12,23,43,235,9,46,3]
+//let i = a.sorted(by: <)
+print(i)
+ */
+
+
+
---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("hello")
+
+var a = [12,23,43,235,9,46,3]
+let i = a.sorted(by: <)
+print(i)
+
+
+var s = [69,10,56,04,19,84]
+let legal = s.sorted(by: >)
+print(legal)
